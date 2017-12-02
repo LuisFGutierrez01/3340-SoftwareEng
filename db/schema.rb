0ddc5ab57295621648f6f9b40f4fa21335fe324b
Kohada-Koheiji <balanza.jose96@gmail.com>
2017-12-02 17:59:01 -0600

    Added basic information to users/ Installed Paperclip for future avatar and file upload.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130191734) do
+ActiveRecord::Schema.define(version: 20171202224243) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,7 +26,10 @@ ActiveRecord::Schema.define(version: 20171130191734) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role_id"
-    t.string "name"
+    t.string "username"
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "birthday"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
