3fa780130a5f3282f0edac2fe9dc7efc196de055
Kohada-Koheiji <balanza.jose96@gmail.com>
2017-12-11 22:41:52 -0600

    Did a little facelift to the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,12 @@ class HomeController < ApplicationController
   def index
     @user = current_user
     @goals = Goal.all
-    if @user
-      @player = Player.find(user_id: @user.id)
-      @goals_own = Goal.where(player_id: @player.id)
+    @players = Player.all
+    if @user != nil
+      @player = @user.player
+      if @user.is_player?
+        @goals_own = Goal.where(player_id: @player.id)
+      end
     end
   end
 
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,7 @@
 class Player < ApplicationRecord
   belongs_to :user
   has_many :goals
+
   def player_exist?(id)
     @player = Player.find(id)
     !@player.nil?
